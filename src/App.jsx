2201121fa912315b5d42c00b30b1fa0496ac92bc
Rez <rezhates@gmail.com>
2025-10-26 15:10:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import React, { useRef, useState } from "react";
-import Section1 from "./sections/Section1";
-import Section2 from "./sections/Section2";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import React from 'react'
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 const App = () => {
-  const containerRef = useRef(null);
-  const [scrollProgress, setScrollProgress] = useState(0);
-  useGSAP(() => {
-    ScrollTrigger.create({
-      trigger: containerRef.current,
-      start: "top top",
-      end: "bottom bottom",
-      scrub: true,
-      onUpdate: (self) => {
-        setScrollProgress(self.progress);
-      },
-    });
-  });
   return (
-    <main ref={containerRef} className="relative bg-black h-[200vh]">
-      <Section1 scrollProgress={scrollProgress} />
+    <div>App</div>
+  )
+}
 
-      <Section2 scrollProgress={scrollProgress} />
-    </main>
-  );
-};
-
-export default App;
+export default App
