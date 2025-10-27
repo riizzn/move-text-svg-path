3d1abfafdb1cb437271db6234c3ac65cb9369bdc
Rez <rezhates@gmail.com>
2025-10-27 21:27:10 +0530

    Correct method to implement

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,62 +1,62 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 const Footer = () => {
   const containerRef = useRef(null);
-  const [scrollProgress, setScrollProgress] = useState(0);
-  const texts = useRef([]);
-  useGSAP(() => {
-    gsap.to(containerRef.current, {
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "top bottom",
-        end: "bottom bottom",
-        scrub: true,
-        onUpdate: (self) => {
-          setScrollProgress(self.progress);
-        },
-      },
-    });
-    console.log(scrollProgress)
+  const texts = useRef([]); // Ref to hold the array of textPath elements
 
-    texts.current.forEach((text, i) => {
-      text.setAttribute("startOffset", -42 + (i*42) + (scrollProgress*40)  + "%");
-
-    });
-  }, [scrollProgress]);
+  useGSAP(
+    () => {
+      texts.current.forEach((text, i) => {
+        gsap.to(text, {
+          // Animate the 'startOffset' attribute
+          attr: {
+            // This is the *end* position
+            startOffset: `${40 * i}%`, // END: 0%, 40%, 80%
+          },
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "top bottom",
+            end: "bottom bottom",
+            scrub: true,
+          },
+        });
+      });
+    },
+    { scope: containerRef }
+  );
 
   return (
-    <div ref={containerRef}>
+    <div ref={containerRef} className="relative">
       <svg className="pb-40" viewBox="0 0 250 90">
         <path
           id="curve"
           fill="none"
           d="m0,88.5c61.37,0,61.5-68,126.5-68,58,0,51,68,123,68"
-          
         />
         <text
-          className="text-[6px] uppercase font-bold  font-playfair tracking-tighter"
+          className="text-[6px] uppercase font-bold font-playfair tracking-tighter"
           style={{ fill: "red" }}
         >
           {[...Array(3)].map((_, i) => (
             <textPath
-              ref={(ref) => (texts.current[i] = ref)}
-              href="#curve"
-              startOffset={` ${40 * i}%`}
               key={i}
+              ref={(el) => (texts.current[i] = el)}
+              href="#curve"
+              // This is the *start* position
+              startOffset={`${40 * i - 40}%`} // START: -40%, 0%, 40%
             >
               Corner of my bed , maybe?
             </textPath>
           ))}
         </text>
       </svg>
-      <div className='h-[250px]'></div>
-      
+
+      <div className="h-[250px]"></div>
     </div>
   );
 };
